204fb1f68a473deb19f9efb76f9f3042a7de5475
Shoriful <shorifulbba0360@gmail.com>
2024-09-24 21:08:22 +0600

    added donate card 1

---
+++ js/utility.js
@@ -0,0 +1,17 @@
+
+
+
+// ************get input Field vlaue by common function********
+
+function getInputValueById (id){
+    parseFloat(document.getElementById(id).value);
+    return;
+}
+
+
+// ************get inner Text vlaue by common function********
+
+function getInputValueById (id){
+    parseFloat(document.getElementById(id).innerText);
+    return;
+}
